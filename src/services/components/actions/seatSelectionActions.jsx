b1256502fca5530fd31ca3e04bc75bb974846bb4
Popuri-Sowmya <166593956+Popuri-Sowmya@users.noreply.github.com>
2024-04-10 14:15:33 +0530

    committed

--- src/services/components/actions/seatSelectionActions.jsx
+++
@@ -1,4 +0,0 @@
-export const updatePassengerNames = (names) => ({
-    type: 'UPDATE_PASSENGER_NAMES',
-    payload: names,
-});
--- src/services/components/actions/userActions.jsx
+++
@@ -1,5 +0,0 @@
-export const UPDATE_SEARCH_DATA = 'UPDATE_SEARCH_DATA';
-export const updateSearchData = (source, destination, date) => ({
-  type: UPDATE_SEARCH_DATA,
-  payload: { source, destination, date },
-});
